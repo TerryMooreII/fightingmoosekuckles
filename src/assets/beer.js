27d457020b218b0c334bffb7032b1029c1a82a21
Terry Moore <terry.moore.ii@gmail.com>
2025-08-25 11:09:52 -0400

    Update beer.js

--- src/assets/beer.js
+++ src/assets/beer.js
@@ -1,13 +1,13 @@
 
 export default `
   Terry, 8/12
-  Kevin, 9/19
+  Kevin, 8/19
+  Brian 8/26,
   Sky, 1/22
   George, 1/28
   Christian, 2/25
   Mark, 3/11
   Barry, 3/18
-  Brian 4/1,
   Vic 4/8,
   Chris, 4/15
   Andrew, 4/29
