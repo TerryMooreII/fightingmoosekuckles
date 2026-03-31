5829f828f550308daf7317e58e78343128646a1d
TerryMooreII <terry@mooreii.com>
2026-03-31 23:56:37 +0000

    Deploying to gh-pages from @ TerryMooreII/fightingmoosekuckles@783dbade5bc2e1b04f745fe752dca526680cac6b 🚀

---
+++ build/p-ad0f168b.entry.js
@@ -0,0 +1,2 @@
+import{r as t,g as s}from"./p-6d7624a9.js";const i=class{constructor(s){t(this,s);this.context={};this.renderer=()=>null}connectedCallback(){if(this.subscribe!=null){this.unsubscribe=this.subscribe(this.el,"context")}}disconnectedCallback(){if(this.unsubscribe!=null){this.unsubscribe()}}render(){return this.renderer(Object.assign({},this.context))}get el(){return s(this)}};export{i as context_consumer};
+//# sourceMappingURL=p-ad0f168b.entry.js.map
