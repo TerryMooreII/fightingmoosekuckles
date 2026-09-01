7f05e6524bbc21daf80fc3ed09e363f40793b94d
TerryMooreII <terry@mooreii.com>
2026-09-01 19:36:35 +0000

    Deploying to gh-pages from @ TerryMooreII/fightingmoosekuckles@baa84fae983fb7e77abcd404bd1ce352979c36ab 🚀

--- assets/beer.js
+++ assets/beer.js
@@ -1,17 +1,17 @@
 
 export default `
   Chris, 5/19
-  Mac, 5/26
   Matt, 6/16
   Andrew, 7/14
   Kevin, 7/21
   Dan, 7/25
   Vic, 7/26,
-  Rick, 7/28
   Terry, 8/4
   Lans, 8/11
   Sills, 8/18
   Christian, 8/25
+  Rick, 9/1
+  Mac, 9/8
   Sky, 4/14
   Barry, 5/12
   Joe, 0/00
